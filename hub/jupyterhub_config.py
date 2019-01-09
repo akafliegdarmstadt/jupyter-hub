1ba88b6d633f62dc5ec703136229c02c7a628fd3
Sebastian Clermont <mail@sebastianclermont.de>
2019-01-09 20:40:43 +0100

    changed auth to github

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -1,4 +1,6 @@
 import os
+from oauthenticator.github import GitHubOAuthenticator
+
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
@@ -13,9 +15,12 @@ c.JupyterHub.services = [
     },
 ]
 
-c.LDAPAuthenticator.server_address = 'ldap.example.com'
-c.LDAPAuthenticator.bind_dn_template = []
-c.JupyterHub.authenticator_class = 'ldapauthenticator.LDAPAuthenticator'
+#c.LDAPAuthenticator.server_address = 'ldap.example.com'
+#c.LDAPAuthenticator.bind_dn_template = []
+#c.JupyterHub.authenticator_class = 'ldapauthenticator.LDAPAuthenticator'
 
-
-
+c.JupyterHub.authenticator_class = GitHubOAuthenticator
+c.GitHubOAuthenticator.oauth_callback_url = 'https://hessen.akaflieg.tu-darmstadt.de/hub/oauth_callback'
+c.GitHubOAuthenticator.client_id = '3fb6064ba4797d2b93fa'
+c.GitHubOAuthenticator.client_secret = '39e18dec48c28ecbff1994122e80eec7d316a4e8'
+c.Authenticator.admin_users = {'nichtmonti'}
